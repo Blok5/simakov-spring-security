e72db7e4079a87bf4908fd5f0babe5ccc94b1b27
igorsimakov <Simakoff10@mail.ru>
2022-02-06 12:54:47 +0300

    Permission Based Authentication

---
+++ src/main/java/com/example/springsecurity/security/ApplicationSecurityConfig.java
@@ -0,0 +1,59 @@
+package com.example.springsecurity.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import static com.example.springsecurity.security.ApplicationUserRole.ADMIN;
+import static com.example.springsecurity.security.ApplicationUserRole.STUDENT;
+
+/**
+ * Created by igorsimakov on 03.02.2022
+ */
+
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
+                .antMatchers("/api/**").hasRole(STUDENT.name())
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails anna = User.builder()
+                .username("anna")
+                .password(passwordEncoder.encode("password"))
+                .roles(STUDENT.name()) //ROLE_STUDENT
+                .build();
+
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder.encode("admin"))
+                .roles(ADMIN.name()) //ROLE_STUDENT
+                .build();
+
+        return new InMemoryUserDetailsManager(anna, admin);
+    }
+}
